cdc17587b82db36fa6acf4428b7c93e061825b04
NinjaLaranj4 <132260498+NinjaLaranj4@users.noreply.github.com>
2023-05-01 07:11:03 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,69 +1,50 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Código anterior
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("comment-form");
+    const comentariosLista = document.getElementById("comentarios-lista");
 
-    // Initialize Firebase Realtime Database (or Firestore)
     const db = firebase.database();
-    // (use this line for Firestore)
-    // const db = firebase.firestore();
 
     function exibirComentarios() {
-        // Get comments from Firebase Realtime Database (or Firestore)
-        db.ref("comentarios").once("value", function(snapshot) {
+        db.ref("comentarios").once("value", function (snapshot) {
             const comentarios = snapshot.val() || [];
             comentariosLista.innerHTML = '';
 
-            comentarios.forEach(comentario => {
-                // Código para exibir comentários (sem alteração)
-            });
+            for (const key in comentarios) {
+                const comentario = comentarios[key];
+                const div = document.createElement("div");
+                div.classList.add("comentario");
+
+                const h3 = document.createElement("h3");
+                h3.textContent = comentario.name;
+                div.appendChild(h3);
+
+                const p = document.createElement("p");
+                p.textContent = comentario.comment;
+                div.appendChild(p);
+
+                comentariosLista.appendChild(div);
+            }
         });
-        // (use these lines for Firestore)
-        // db.collection("comentarios").get().then((querySnapshot) => {
-        //     comentariosLista.innerHTML = '';
-        //
-        //     querySnapshot.forEach((doc) => {
-        //         const comentario = doc.data();
-        //         // Código para exibir comentários (sem alteração)
-        //     });
-        // });
     }
 
-    form.addEventListener("submit", function(event) {
+    form.addEventListener("submit", function (event) {
         event.preventDefault();
 
         const name = document.getElementById("name").value;
         const comment = document.getElementById("comment").value;
 
-                // Save comment to Firebase Realtime Database (or Firestore)
         const newCommentRef = db.ref("comentarios").push();
-        newCommentRef.set({ name, comment }, function(error) {
+        newCommentRef.set({ name, comment }, function (error) {
             if (error) {
                 console.error("Erro ao salvar o comentário:", error);
             } else {
-                // Limpar campos do formulário
                 document.getElementById("name").value = "";
                 document.getElementById("comment").value = "";
 
                 exibirComentarios();
             }
         });
-
-        // (use these lines for Firestore)
-        // db.collection("comentarios").add({
-        //     name: name,
-        //     comment: comment
-        // })
-        // .then((docRef) => {
-        //     // Limpar campos do formulário
-        //     document.getElementById("name").value = "";
-        //     document.getElementById("comment").value = "";
-        //
-        //     exibirComentarios();
-        // })
-        // .catch((error) => {
-        //     console.error("Erro ao salvar o comentário:", error);
-        // });
     });
 
     exibirComentarios();
 });
-
